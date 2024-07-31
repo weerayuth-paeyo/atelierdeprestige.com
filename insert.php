038a6fafe349725123aca2428d0105213b562af2
Park <weerayuth.paeyo@gmail.com>
2024-07-31 10:13:08 +0700

    send mail

--- insert.php
+++ insert.php
@@ -1,6 +1,7 @@
 <?php
 // Include the database connection file
 require_once 'connect_db.php';
+require_once 'EmailSender.php';
 
 // Get the database connection
 $db = Database::getInstance();
@@ -20,21 +21,28 @@ $birth_date = $_POST['year'] . '-'. $_POST['month'] . '-'. $_POST['day'];
 $created_at = date('Y-m-d H:i:s');
 $updated_at = $created_at;
 
-
-
 // Prepare SQL statement
 $stmt = $conn->prepare("INSERT INTO contact (firstname, lastname, phone, store, birth_date, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("sssssss", $firstname, $lastname, $phone, $store, $birth_date, $created_at, $updated_at);
 
-// // Execute the statement
-// if ($stmt->execute()) {
-//     echo "New record created successfully";
-// } else {
-//     echo "Error: " . $stmt->error;
-// }
-
 // Execute the statement
 if ($stmt->execute()) {
+    $mail_host = "mail.atelierdeprestige.com";
+    $mail_username = "support@atelierdeprestige.com";
+    $mail_password = "1234512345";
+    $mail_port = 587;
+    $emailSender = new EmailSender($mail_host, $mail_username, $mail_password);
+    // ส่งอีเมล adpcampaignlandingpage@gmail.com
+    $result = $emailSender->sendEmail(
+        'support@atelierdeprestige.com', 
+        'Atelierd Support', 
+        'adpcampaignlandingpage@gmail.com', 
+        'Owner', 
+        'Contact is signed.', 
+        'Customer <br>Name : ' . $firstname . ' ' . $lastname . ' <br>Email : ' . $email . ' <br>Phone : ' . $phone . ' <br>Store : ' . $store . '',
+        'This is a test email sent from PHP using PHPMailer.'
+    );
+    
     // Redirect to atelierdeprestige.com with a success message
     header("Location: http://atelierdeprestige.com?status=success");
     exit();
